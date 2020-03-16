395df5cc439a3e84d2649aa9d75d4efd8ffb9693
Yoga Sukma <hello@yogasukma.web.id>
2020-03-16 22:50:34 +0800

    adding way to test RSS url

--- app/Console/Commands/GrabNews.php
+++ app/Console/Commands/GrabNews.php
@@ -9,10 +9,11 @@ class GrabNews extends Command
 {
     /**
      * The name and signature of the console command.
+     * url parameter is optional, when it used, it will only test to grab the news, not save it.
      *
      * @var string
      */
-    protected $signature = 'grab:news';
+    protected $signature = 'grab:news {url?}';
 
     /**
      * The console command description.
@@ -36,6 +37,27 @@ class GrabNews extends Command
      */
     public function handle()
     {
+        // if artisan has $url parameter, then use it as test, 
+        // not save it.
+        if(!is_null($this->argument("url"))) {
+            $this->testRssUrl($this->argument("url"));
+
+            return false;
+        }
+
+        // run the scrappers
+        // save the results.
         (new NewsSeeker())->get();
+    }
+
+    protected function testRssUrl($url)
+    {
+        $feeds = (new NewsSeeker())->getFeed($url);
+
+        if ($feeds) {
+            foreach ($feeds->get_items() as $feed) {
+                $this->info($feed->get_title());
+            }
+        }
     }
 }
